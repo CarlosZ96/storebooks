e304fa1b5ce0582bb5cf219aeacf67895aaf8b58
CarlosZ96 <c4r1oszambrano@gmail.com>
2023-08-25 16:52:07 -0500

    Feature structure:
    React Router is configured and two routes are added: "/" and "/categories". The "Addnewbooks" and "Books" components and their content, buttons, selector paragraphs, etc. are created.

---
+++ src/components/newbook.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../StyleSheets/newbook.css';
+
+
+const AddNewBooks = () => {
+  return (
+    <div className="add-new-book">
+      <input type="text" className="new-book-input" placeholder="Book Tittle" />
+      <form className="categorie">
+        <select>
+          <option >Action</option>
+          <option >Romance</option>
+          <option >Fantasy</option>
+          <option >Mystery</option>
+        </select>
+        <button type="submit">Add Book</button>
+      </form>
+    </div>
+  )
+}
+
+export default AddNewBooks;
