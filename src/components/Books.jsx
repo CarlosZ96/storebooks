1eae0beddc0470f14659e6d3cf26b6823a1baa88
CarlosZ96 <c4r1oszambrano@gmail.com>
2023-08-24 20:39:54 -0500

    Update:
    Changing position of the divisions.

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -5,29 +5,31 @@ import '../StyleSheets/books.css';
 const Books = () => {
   return (
     <div className="book-container">
-      <section className="tittle-circle-container">
-        <div className="book-info">
-          <p>Categorie</p>
-          <h2>Book example</h2>
-          <p>Author</p>
-          <ul>
-            <li><button>Comments</button></li>
-            <li><button>Remove</button></li>
-            <li><button>Edit</button></li>
-          </ul>
-        </div>
-        <div className="percentage-container">
-          <img src="https://cdn-icons-png.flaticon.com/128/5698/5698579.png" alt="50% cirlcle" />
-          <div className="Percentage-text-container">
-            <p>Completed</p>
+      <div className="cards">
+        <section className="tittle-circle-container">
+          <div className="book-info">
+            <p>Categorie</p>
+            <h2>Book example</h2>
+            <p>Author</p>
+            <ul>
+              <li><button>Comments</button></li>
+              <li><button>Remove</button></li>
+              <li><button>Edit</button></li>
+            </ul>
           </div>
-        </div>
-      </section>
-      <section className="current-chapter-container">
-        <p>Current Chapter</p>
-        <p>Chapter example</p>
-        <button>Update Progress</button>
-      </section>
+          <div className="percentage-container">
+            <img src="https://cdn-icons-png.flaticon.com/128/5698/5698579.png" alt="50% cirlcle" />
+            <div className="Percentage-text-container">
+              <p>Completed</p>
+            </div>
+          </div>
+        </section>
+        <section className="current-chapter-container">
+          <p>Current Chapter</p>
+          <p>Chapter example</p>
+          <button>Update Progress</button>
+        </section>
+      </div>
       <AddNewBooks />
     </div>
   );
