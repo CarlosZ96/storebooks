bc5741e0b895a08cb34b9b19f3d746883241e625
CarlosZ96 <c4r1oszambrano@gmail.com>
2023-09-08 14:39:19 -0500

    Update booksSlice.js

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -9,7 +9,9 @@ export const getBooks = createAsyncThunk(
       const response = await axios.get(
         'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/JX6HgfhVoknuk7ZGztbn/books',
       );
+      console.log(response);
       const data = response.data;
+      console.log(data);
       const booksIds = Object.keys(data);
       const booksApi = [];
       booksIds.forEach((bookId) => {
